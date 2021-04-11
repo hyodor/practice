22d1ae1e98d23de52f4ed77ee1b610fb9003763c
hyodor <jshk1216@gmail.com>
2021-04-11 21:39:44 +0900

    add Hello java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,25 @@
 class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++)
-           System.out.println(args[i]);
+        String example = "Hello world No.1 champion 2021 bye 2024";
+        int sum = 0;
+        String temp = "";
+
+        for (int i = 0; i < example.length(); i++) {
+            if (example.charAt(i) >= '0' && example.charAt(i) <= '9') {
+                temp += example.charAt(i);
+            } else {
+                if (temp.length() > 0) {
+                    sum += Integer.parseInt(temp);
+                    temp = "";
+                }
+            }
+        }
+
+        if (temp.length() > 0) {
+            sum += Integer.parseInt(temp);
+            temp = "";
+        }
+
+        System.out.println(sum);
     }
 }
